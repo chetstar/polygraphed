ec2c6dd5ad9024e2b87cd92a1b659caae69eab2f
chetstar <cmeinzer@acbhcs.org>
2014-05-08 23:02:42 -0700

    api key config import

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 THE_API_KEY = str(os.environ.get('theapikey'))
+_API_KEY = str(os.environ.get('theapikey'))
 #variables for ingestion by flask app
 # from apikey import _API_KEY
 API_KEY = THE_API_KEY
